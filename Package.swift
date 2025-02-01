47a6918e343b621a81cbd534b9fc29e86a460edc
Gustaf Rosenblad <gustaf@ccode.se>
2025-02-01 11:55:43 +0100

    feat: Added gRPC package providing a client interceptor for adding auth headers

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "google-cloud-auth",
   platforms: [
-    .macOS(.v13)
+    .macOS(.v15)
   ],
   products: [
     .library(name: "GoogleCloudAuth", targets: ["GoogleCloudAuth"]),
@@ -16,7 +16,8 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.54.0"),
     .package(url: "https://github.com/rosecoder/retryable-task.git", from: "1.1.2"),
     .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
-    .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0-rc.2"),
+    .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0"),
+    .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
   ],
   targets: [
     .target(
@@ -36,6 +37,14 @@ let package = Package(
       name: "GoogleCloudAuthTesting",
       dependencies: [
         .target(name: "GoogleCloudAuth")
+      ]
+    ),
+
+    .target(
+      name: "GoogleCloudAuthGRPC",
+      dependencies: [
+        .target(name: "GoogleCloudAuth"),
+        .product(name: "GRPCCore", package: "grpc-swift"),
       ]
     ),
   ]
